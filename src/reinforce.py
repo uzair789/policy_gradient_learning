10380150bf30e0cc277b39bf2ef36b1531093382
uzair.789 <uzair.789@hotmail.com>
2019-10-08 14:50:14 -0400

    model created and generating episodes

--- src/reinforce.py
+++ src/reinforce.py
@@ -3,12 +3,39 @@ import argparse
 import numpy as np
 import tensorflow as tf
 import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
 import gym
 
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
+
+def create_model():
+    """ This function create the NN using keras library.
+    """
+   
+    model = Sequential()
+    model.add(Dense(16,
+                    input_shape=(8,),
+                    activation='relu',
+                    kernel_initializer=keras.initializers.VarianceScaling(scale=1.0, distribution='uniform', mode='fan_avg'),
+                    bias_initializer=keras.initializers.Ones()))
+    model.add(Dense(16,
+                    activation='relu',
+                    kernel_initializer=keras.initializers.VarianceScaling(scale=1.0, distribution='uniform', mode='fan_avg'),
+                    bias_initializer=keras.initializers.Ones()))
+    model.add(Dense(16,
+                    activation='relu',
+                    kernel_initializer=keras.initializers.VarianceScaling(scale=1.0, distribution='uniform', mode='fan_avg'),
+                    bias_initializer=keras.initializers.Ones()))
+    model.add(Dense(4,
+                    activation='softmax',
+                    kernel_initializer=keras.initializers.VarianceScaling(scale=1.0, distribution='uniform', mode='fan_avg'),
+                    bias_initializer=keras.initializers.Ones()))
+    return model
 
 class Reinforce(object):
     # Implementation of the policy gradient method REINFORCE.
@@ -18,12 +45,18 @@ class Reinforce(object):
 
         # TODO: Define any training operations and optimizers here, initialize
         #       your variables, or alternately compile your model here.
+        self.model.compile(loss='categorical_crossentropy',
+                           optimizer=keras.optimizers.Adam(lr),
+                           metrics=['accuracy'])
+
 
     def train(self, env, gamma=1.0):
         # Trains the model on a single episode using REINFORCE.
         # TODO: Implement this method. It may be helpful to call the class
         #       method generate_episode() to generate training data.
         return
+
+
 
     def generate_episode(self, env, render=False):
         # Generates an episode by executing the current policy in the given env.
@@ -35,7 +68,20 @@ class Reinforce(object):
         states = []
         actions = []
         rewards = []
+        done = False
+        state = env.reset()
+        while not done:
+            # replace random action with policy
+            if render:
+                env.render()
+            action = env.action_space.sample()
+            next_state, reward, done, info = env.step(action)
+            print(state)	
+            states.append(state)
+            actions.append(action)
+            rewards.append(reward)
 
+            state = next_state
         return states, actions, rewards
 
 
@@ -64,11 +110,18 @@ def main(args):
     # Parse command-line arguments.
     args = parse_arguments()
     num_episodes = args.num_episodes
-    lr = args.lr
+    lr = 0.001 #args.lr
     render = args.render
 
     # Create the environment.
     env = gym.make('LunarLander-v2')
+
+    # Create the model
+    model = create_model()
+    model.get_config()
+    re = Reinforce(model, lr)
+    for i in range(10):
+        s, a, r = re.generate_episode(env, True) 
 
     # TODO: Create the model.
 
